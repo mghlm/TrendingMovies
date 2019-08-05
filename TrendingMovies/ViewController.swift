4fa5ca45a44c8ff77d7d7acc7aaff214334768ad
mghlm <mail@magnusholm.com>
2019-08-05 17:03:04 +0100

    Initial Commit

---
+++ TrendingMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrendingMovies
+//
+//  Created by Magnus Holm on 05/08/2019.
+//  Copyright © 2019 Magnus Holm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
